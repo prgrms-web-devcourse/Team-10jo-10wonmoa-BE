cd87bea977392120d92fb3f8c51abd68e6f363ff
jki503 <jki503@naver.com>
2022-07-25 23:40:57 +0900

    Test: ExpenditureServiceTest 작성

--- src/test/java/com/prgrms/tenwonmoa/domain/accountbook/service/ExpenditureServiceTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/accountbook/service/ExpenditureServiceTest.java
@@ -1,5 +1,12 @@
 package com.prgrms.tenwonmoa.domain.accountbook.service;
 
+import static java.util.Optional.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.time.LocalDate;
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,10 +15,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.prgrms.tenwonmoa.domain.accountbook.Expenditure;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.CreateExpenditureRequest;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.CreateExpenditureResponse;
 import com.prgrms.tenwonmoa.domain.accountbook.repository.ExpenditureRepository;
+import com.prgrms.tenwonmoa.domain.category.Category;
+import com.prgrms.tenwonmoa.domain.category.CategoryType;
+import com.prgrms.tenwonmoa.domain.category.UserCategory;
 import com.prgrms.tenwonmoa.domain.category.repository.CategoryRepository;
 import com.prgrms.tenwonmoa.domain.category.repository.UserCategoryRepository;
+import com.prgrms.tenwonmoa.domain.user.User;
 import com.prgrms.tenwonmoa.domain.user.repository.UserRepository;
+import com.prgrms.tenwonmoa.exception.message.Message;
 
 @DisplayName("Expenditure(지출) 서비스 계층 단위 테스트")
 @ExtendWith(MockitoExtension.class)
@@ -32,29 +47,68 @@ class ExpenditureServiceTest {
 	@InjectMocks
 	private ExpenditureService expenditureService;
 
+	User user = new User("jungki111@gmail,com", "password1234!", "개발자");
+
+	UserCategory userCategory = new UserCategory(user, new Category("식비", CategoryType.EXPENDITURE));
+
+	Category category = new Category("식비", CategoryType.EXPENDITURE);
+
+	Expenditure expenditure = new Expenditure(LocalDate.now(), 10000L, "피자", category.getName(), user, userCategory);
+
 	@Nested
 	class 지출_생성_중 {
 
+		Long userId = 1L;
+		CreateExpenditureRequest request = new CreateExpenditureRequest(LocalDate.now(), 10000L, "식비", 1L);
+
 		@Test
 		public void User가_없을_경우() {
+			given(userRepository.findById(userId)).willThrow(
+				new NoSuchElementException(Message.USER_NOT_FOUND.getMessage()));
 
+			assertThatThrownBy(() -> expenditureService.createExpenditure(userId, any()))
+				.isInstanceOf(NoSuchElementException.class)
+				.hasMessage(Message.USER_NOT_FOUND.getMessage());
 		}
 
 		@Test
 		public void User_Category가_없을_경우() {
+			given(userRepository.findById(any())).willReturn(of(user));
+			given(userCategoryRepository.findById(request.getUserCategoryId()))
+				.willThrow(new NoSuchElementException(Message.USER_CATEGORY_NOT_FOUND.getMessage()));
+
+			assertThatThrownBy(() -> expenditureService.createExpenditure(any(), request))
+				.isInstanceOf(NoSuchElementException.class)
+				.hasMessage(Message.USER_CATEGORY_NOT_FOUND.getMessage());
 
 		}
 
 		@Test
 		public void Category가_없을_경우() {
+			given(userRepository.findById(any())).willReturn(ofNullable(user));
+			given(userCategoryRepository.findById(request.getUserCategoryId()))
+				.willReturn(of(userCategory));
 
+			given(categoryRepository.findById(userCategory.getCategory().getId()))
+				.willThrow(new NoSuchElementException(Message.CATEGORY_NOT_FOUND.getMessage()));
+
+			assertThatThrownBy(() -> expenditureService.createExpenditure(any(), request))
+				.isInstanceOf(NoSuchElementException.class)
+				.hasMessage(Message.CATEGORY_NOT_FOUND.getMessage());
 		}
 
+		@Test
+		public void 성공적으로_controller에_응답을_반환한다() {
+			given(userRepository.findById(userId)).willReturn(ofNullable(user));
+			given(userCategoryRepository.findById(request.getUserCategoryId()))
+				.willReturn(of(userCategory));
+			given(categoryRepository.findById(any()))
+				.willReturn(of(category));
+			given(expenditureRepository.save(any(Expenditure.class))).willReturn(expenditure);
+
+			CreateExpenditureResponse response = expenditureService.createExpenditure(userId, request);
+
+			assertThat(response.getId()).isEqualTo(expenditure.getId());
+		}
 	}
-
-	@Test
-	public void 성공적으로_controller에_응답을_반환한다() {
-
-	}
-
 }

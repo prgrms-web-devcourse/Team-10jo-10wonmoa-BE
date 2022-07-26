25b3eac919858d7efad2bb41f0e723650b2fdf02
jki503 <jki503@naver.com>
2022-07-26 15:14:54 +0900

    fix: bean validation 변수타입으로 넣을 경우 컴파일 실패해서 매직넘버로 변경

--- src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/CreateExpenditureRequest.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/CreateExpenditureRequest.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
-import com.prgrms.tenwonmoa.domain.accountbook.AccountBookConst;
 import com.prgrms.tenwonmoa.domain.accountbook.Expenditure;
 import com.prgrms.tenwonmoa.domain.category.UserCategory;
 import com.prgrms.tenwonmoa.domain.user.User;
@@ -16,8 +15,8 @@ public class CreateExpenditureRequest {
 	@NotNull
 	private final LocalDate registerDate;
 
-	@Min(AccountBookConst.AMOUNT_MIN)
-	@Max(AccountBookConst.AMOUNT_MAX)
+	@Min(0L)
+	@Max(1000000000000L)
 	private final Long amount;
 
 	private final String content;

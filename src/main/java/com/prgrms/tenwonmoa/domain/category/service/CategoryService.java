5a9994cd1fd434ed97ff49960978d62417af7fb6
geuno <rlfrmsdh1@gmail.com>
2022-07-26 18:11:48 +0900

    feat: optional orelseThrow

--- src/main/java/com/prgrms/tenwonmoa/domain/category/service/CategoryService.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/category/service/CategoryService.java
@@ -4,7 +4,6 @@ import static com.prgrms.tenwonmoa.exception.message.Message.*;
 
 import java.util.Locale;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -43,13 +42,10 @@ public class CategoryService {
 	}
 
 	public String updateName(User user, Long categoryId, String name) {
-		Optional<UserCategory> optionalUserCategory =
-			userCategoryRepository.findByUserAndCategory(user.getId(), categoryId);
-		if (optionalUserCategory.isEmpty()) {
-			throw new NoSuchElementException(USER_CATEGORY_NOT_FOUND.getMessage());
-			// 이것도 정상이 아닌 공격 or 버그
-			// -> 개발자가 알아야 할 예외(클라이언트에게는 잘못된 요청이라고 주고, 우리가 알아야 할 예외임)
-		}
+		userCategoryRepository.findByUserAndCategory(user.getId(), categoryId)
+			.orElseThrow(() -> new NoSuchElementException(USER_CATEGORY_NOT_FOUND.getMessage()));
+		// 이것도 정상이 아닌 공격 or 버그
+		// -> 개발자가 알아야 할 예외(클라이언트에게는 잘못된 요청이라고 주고, 우리가 알아야 할 예외임)
 
 		Category category = findCategoryById(categoryId);
 		category.updateName(name);

2727e29bf825ddf59c707a48cca2e5f48dd038a0
geuno <rlfrmsdh1@gmail.com>
2022-07-26 17:10:24 +0900

    feat: 카테고리 수정 구현  - UserCategoryRepository findByUserAndCategory 추가

--- src/main/java/com/prgrms/tenwonmoa/domain/category/repository/UserCategoryRepository.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/category/repository/UserCategoryRepository.java
@@ -1,11 +1,19 @@
 package com.prgrms.tenwonmoa.domain.category.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.prgrms.tenwonmoa.domain.category.UserCategory;
 
 @Repository
 public interface UserCategoryRepository extends JpaRepository<UserCategory, Long> {
+
+	@Query(value = "select u from UserCategory u"
+		+ " where u.user.id = :userId"
+		+ " and u.category.id =:categoryId")
+	Optional<UserCategory> findByUserAndCategory(Long userId, Long categoryId);
 
 }
--- src/test/java/com/prgrms/tenwonmoa/domain/category/service/CategoryServiceTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/category/service/CategoryServiceTest.java
@@ -3,6 +3,7 @@ package com.prgrms.tenwonmoa.domain.category.service;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.prgrms.tenwonmoa.common.fixture.Fixture;
+import com.prgrms.tenwonmoa.domain.category.UserCategory;
 import com.prgrms.tenwonmoa.domain.category.dto.service.SingleCategoryResult;
+import com.prgrms.tenwonmoa.domain.category.repository.UserCategoryRepository;
 import com.prgrms.tenwonmoa.domain.user.User;
 import com.prgrms.tenwonmoa.domain.user.repository.UserRepository;
 
@@ -28,6 +31,10 @@ class CategoryServiceTest {
 	private UserRepository userRepository;
 	// TODO : UserService 구현 시, UserService로 교체
 
+	@Autowired
+	private UserCategoryRepository userCategoryRepository;
+	// TODO : UserCategoryService로 리팩토링 시, UserCategoryService로 교체
+
 	@BeforeEach
 	void setup() {
 		userRepository.save(user);
@@ -41,13 +48,45 @@ class CategoryServiceTest {
 
 		//when
 		Long categoryId = service.register(user, categoryType, categoryName);
-		SingleCategoryResult category = service.getById(categoryId);
 
 		//then
+		SingleCategoryResult category = service.getById(categoryId);
 		assertThat(category).extracting(
 				SingleCategoryResult::getName,
 				SingleCategoryResult::getType)
 			.isEqualTo(List.of(categoryName, categoryType));
 	}
 
+	@Test
+	void 카테고리_이름_업데이트() {
+		//given
+		String categoryType = "EXPENDITURE";
+		String categoryName = "예시지출카테고리";
+		Long categoryId = service.register(user, categoryType, categoryName);
+
+		//when
+		service.updateName(user, categoryId, "업데이트된카테고리");
+
+		//then
+		SingleCategoryResult categoryResult = service.getById(categoryId);
+		assertThat(categoryResult.getName()).isEqualTo("업데이트된카테고리");
+	}
+
+	@Test
+	void 유저_카테고리에_존재하지_않을시_업데이트_오류() {
+		//given
+		String categoryType = "EXPENDITURE";
+		String categoryName = "예시지출카테고리";
+		Long categoryId = service.register(user, categoryType, categoryName);
+
+		UserCategory userCategory =
+			userCategoryRepository.findByUserAndCategory(user.getId(), categoryId).get();
+		userCategoryRepository.delete(userCategory);
+
+		//when
+		//then
+		assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(
+			() -> service.updateName(user, categoryId, "업데이트된카테고리")
+		);
+	}
 }

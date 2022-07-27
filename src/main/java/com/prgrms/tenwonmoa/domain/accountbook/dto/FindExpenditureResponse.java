7b253af32306f32beff509537d05dea8199b30e5
jki503 <jki503@naver.com>
2022-07-27 20:55:28 +0900

    fix: reponse 명세 수정, categoryName 누락

--- src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/FindExpenditureResponse.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/FindExpenditureResponse.java
@@ -6,27 +6,36 @@ import com.prgrms.tenwonmoa.domain.accountbook.Expenditure;
 
 public class FindExpenditureResponse {
 
+	private final Long id;
+
 	private final LocalDate registerDate;
 
 	private final Long amount;
 
 	private final String content;
 
-	private final Long userCategoryId;
+	private final Long categoryId;
 
-	private FindExpenditureResponse(LocalDate registerDate, Long amount, String content, Long userCategoryId) {
+	private final String categoryName;
+
+	private FindExpenditureResponse(Long id, LocalDate registerDate, Long amount, String content, Long categoryId,
+		String categoryName) {
+		this.id = id;
 		this.registerDate = registerDate;
 		this.amount = amount;
 		this.content = content;
-		this.userCategoryId = userCategoryId;
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
 	}
 
 	public static FindExpenditureResponse of(Expenditure expenditure) {
 		return new FindExpenditureResponse(
+			expenditure.getId(),
 			expenditure.getRegisterDate(),
 			expenditure.getAmount(),
 			expenditure.getContent(),
-			expenditure.getUserCategory().getId()
+			expenditure.getUserCategory().getId(),
+			expenditure.getCategoryName()
 		);
 	}
 }

7f37ccc0f23924d1c60791ddf21f8a496d84d0d0
jki503 <jki503@naver.com>
2022-07-27 17:06:58 +0900

    refactor: 지출 생성 response 생성자 private으로 변경

--- src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/CreateExpenditureResponse.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/CreateExpenditureResponse.java
@@ -9,7 +9,7 @@ public class CreateExpenditureResponse {
 
 	private final Long id;
 
-	public CreateExpenditureResponse(Long id) {
+	private CreateExpenditureResponse(Long id) {
 		this.id = id;
 	}
 

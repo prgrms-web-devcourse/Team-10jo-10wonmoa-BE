8403f78cbd3bd0169011c67bd4dd4e2d209e0b62
yanJuicy <hanjub3@gmail.com>
2022-07-27 17:18:42 +0900

    feat(UserRepository): findByEmail 메소드 추가

--- src/main/java/com/prgrms/tenwonmoa/domain/user/repository/UserRepository.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/user/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.prgrms.tenwonmoa.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prgrms.tenwonmoa.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	Optional<User> findByEmail(String email);
 }

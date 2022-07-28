00e13aab44a014d2ad3e4e2a3acf29de7b5481e2
yanJuicy <hanjub3@gmail.com>
2022-07-28 16:45:16 +0900

    feat(GlobalExceptionHandler): DTO validation을 handling 하는 메소드 추가

--- src/main/java/com/prgrms/tenwonmoa/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/tenwonmoa/exception/handler/GlobalExceptionHandler.java
@@ -4,9 +4,13 @@ import static org.springframework.http.HttpStatus.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -48,4 +52,22 @@ public class GlobalExceptionHandler {
 			.status(BAD_REQUEST.value())
 			.body(errorResponse);
 	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleAlreadyExistException(MethodArgumentNotValidException exception) {
+		log.error(exception.getMessage(), exception);
+
+		BindingResult bindingResult = exception.getBindingResult();
+		List<String> errors = bindingResult.getAllErrors()
+			.stream()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.collect(Collectors.toList());
+
+		ErrorResponse errorResponse = new ErrorResponse(errors, BAD_REQUEST.value());
+
+		return ResponseEntity
+			.status(BAD_REQUEST)
+			.body(errorResponse);
+	}
+
 }

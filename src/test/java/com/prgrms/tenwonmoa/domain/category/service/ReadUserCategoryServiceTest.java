d0e2053483b45a91df0fb89cc494e99774aea92a
geuno <rlfrmsdh1@gmail.com>
2022-07-29 18:26:37 +0900

    feat: 카테고리 조회 테스트 케이스 추가

--- src/test/java/com/prgrms/tenwonmoa/domain/category/service/ReadUserCategoryServiceTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/category/service/ReadUserCategoryServiceTest.java
@@ -3,6 +3,7 @@ package com.prgrms.tenwonmoa.domain.category.service;
 import static com.prgrms.tenwonmoa.common.fixture.Fixture.*;
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.prgrms.tenwonmoa.domain.category.dto.service.ReadCategoryResult;
 import com.prgrms.tenwonmoa.domain.category.dto.service.ReadCategoryResult.SingleCategoryResult;
+import com.prgrms.tenwonmoa.domain.category.repository.UserCategoryRepository;
 import com.prgrms.tenwonmoa.domain.user.User;
 import com.prgrms.tenwonmoa.domain.user.repository.UserRepository;
 
@@ -27,11 +29,20 @@ class ReadUserCategoryServiceTest {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private UserCategoryRepository userCategoryRepository;
+
 	private User user;
 
 	@BeforeEach
 	void setup() {
 		user = userRepository.save(createUser());
+	}
+
+	@AfterEach
+	void tearDown() {
+		userCategoryRepository.deleteAll();
+		userRepository.deleteAll();
 	}
 
 	@Test
@@ -58,5 +69,18 @@ class ReadUserCategoryServiceTest {
 		assertThat(incomeCategories.getCategories())
 			.extracting(SingleCategoryResult::getName)
 			.containsExactlyInAnyOrder("월급", "투자");
+	}
+
+	@Test
+	void 잘못된_카테고리_타입_입력시_조회실패() {
+		//given
+		userCategoryService.register(user, "EXPENDITURE", "식비");
+		userCategoryService.register(user, "INCOME", "월급");
+
+		//when
+		//then
+		assertThatIllegalArgumentException().isThrownBy(
+			() -> readUserCategoryService.getUserCategories(user.getId(), "NOT_VALID")
+		);
 	}
 }

73404cb8b04d4892a35a3e42611931c64bb08383
jki503 <jki503@naver.com>
2022-07-30 22:35:16 +0900

    feat: TestConfig에 AccountBookQueryRepository bean 등록

--- src/test/java/com/prgrms/tenwonmoa/config/TestConfig.java
+++ src/test/java/com/prgrms/tenwonmoa/config/TestConfig.java
@@ -6,6 +6,7 @@ import javax.persistence.PersistenceContext;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import com.prgrms.tenwonmoa.domain.accountbook.repository.AccountBookQueryRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @TestConfiguration
@@ -17,5 +18,10 @@ public class TestConfig {
 	@Bean
 	public JPAQueryFactory jpaQueryFactory() {
 		return new JPAQueryFactory(entityManager);
+	}
+
+	@Bean
+	public AccountBookQueryRepository accountBookQueryRepository(JPAQueryFactory jpaQueryFactory) {
+		return new AccountBookQueryRepository(jpaQueryFactory);
 	}
 }

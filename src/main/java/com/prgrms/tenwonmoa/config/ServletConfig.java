e82cd4c03203e16891c636068f4c795c781bb307
geuno <rlfrmsdh1@gmail.com>
2022-07-31 22:29:41 +0900

    feat: CORS 설정 추가

--- src/main/java/com/prgrms/tenwonmoa/config/ServletConfig.java
+++ src/main/java/com/prgrms/tenwonmoa/config/ServletConfig.java
@@ -8,8 +8,7 @@ public class ServletConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-			.allowedOrigins("*")
+			.allowedOrigins("http://localhost:3000", "https://team-10jo-10wonmoa-fe.vercel.app")
 			.allowedMethods("*");
-		// allowedHeaders().allowCredentials()
 	}
 }

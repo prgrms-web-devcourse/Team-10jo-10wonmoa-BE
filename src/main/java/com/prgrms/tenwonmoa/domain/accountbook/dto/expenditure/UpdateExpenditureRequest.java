c5eac77036b26c0e89cb8640d6a0db059165a8ba
geuno <rlfrmsdh1@gmail.com>
2022-08-01 20:56:25 +0900

    feat: Request dto의 bean validation error message 추가

--- src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/expenditure/UpdateExpenditureRequest.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/dto/expenditure/UpdateExpenditureRequest.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.prgrms.tenwonmoa.domain.accountbook.Expenditure;
 import com.prgrms.tenwonmoa.domain.category.UserCategory;
@@ -15,16 +16,17 @@ import lombok.Getter;
 @Getter
 public class UpdateExpenditureRequest {
 
-	@NotNull
+	@NotNull(message = "등록일을 채워주세요")
 	private final LocalDateTime registerDate;
 
-	@Min(0L)
-	@Max(1_000_000_000_000L)
+	@Min(value = 0L, message = "최소값은 0입니다")
+	@Max(value = 1_000_000_000_000L, message = "최대값은 1조입니다")
 	private final Long amount;
 
+	@Size(max = 50, message = "내용의 최대 길이는 50입니다")
 	private final String content;
 
-	@NotNull
+	@NotNull(message = "유저 카테고리 아이디를 채워주세요")
 	private final Long userCategoryId;
 
 	public UpdateExpenditureRequest(LocalDateTime registerDate, Long amount, String content, Long userCategoryId) {

b08d708f54d1f1790aa7e16d4edbd2ebeecbe9e2
yanJuicy <hanjub3@gmail.com>
2022-08-03 17:47:09 +0900

    refactor(V1_0_2__refresh_token_add.sql): email을 primary key로 변경

--- src/main/resources/db/migration/V1_0_2__refresh_token_add.sql
+++ src/main/resources/db/migration/V1_0_2__refresh_token_add.sql
@@ -1,7 +1,6 @@
 create table refresh_token
 (
-    id    bigint       not null auto_increment,
     email varchar(255) not null unique,
     token varchar(255) not null unique,
-    primary key (id)
+    primary key (email)
 );

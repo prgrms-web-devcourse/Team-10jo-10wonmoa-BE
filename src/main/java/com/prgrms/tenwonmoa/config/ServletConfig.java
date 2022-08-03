579a343329e07f2de93ca6873ac6a13dda7a086e
taesan <pawer02023@gmail.com>
2022-08-03 12:29:48 +0900

    refactor: cors 처리
    - cors 처리

--- src/main/java/com/prgrms/tenwonmoa/config/ServletConfig.java
+++ src/main/java/com/prgrms/tenwonmoa/config/ServletConfig.java
@@ -7,11 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ServletConfig implements WebMvcConfigurer {
 
+	public static final String SWAGGER_UI_HOST = "http://3.39.184.232:8082";
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
 			.allowCredentials(true)
 			.allowedOrigins("http://localhost:3000", "https://team-10jo-10wonmoa-fe.vercel.app")
+			.allowedMethods("*")
+			.allowedHeaders("*");
+
+		registry.addMapping("/docs/**")
+			.allowCredentials(true)
+			.allowedOrigins(SWAGGER_UI_HOST)
 			.allowedMethods("*")
 			.allowedHeaders("*");
 	}

5499a761763ddec7a03554b7d4cfa55d5ac2ef0e
yanJuicy <hanjub3@gmail.com>
2022-08-05 04:03:24 +0900

    feat(TokenProvider): 토큰이 만료됐는지 확인하는 로직 추가

--- src/main/java/com/prgrms/tenwonmoa/domain/user/security/jwt/service/TokenProvider.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/user/security/jwt/service/TokenProvider.java
@@ -6,8 +6,11 @@ import java.util.Date;
 import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.prgrms.tenwonmoa.domain.user.security.jwt.Jwt;
+import com.prgrms.tenwonmoa.exception.InvalidTokenException;
+import com.prgrms.tenwonmoa.exception.message.Message;
 
 import lombok.RequiredArgsConstructor;
 
@@ -54,6 +57,18 @@ public class TokenProvider {
 		Jwt.Claims claims = verifyAndGetClaims(token);
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return sdf.format(claims.getExp());
+	}
+
+	public boolean isTokenExpired(String token) {
+		try {
+			jwt.getJwtVerifier().verify(token);
+		} catch (TokenExpiredException te) {
+			return true;
+		} catch (Exception e) {
+			throw new InvalidTokenException(Message.INVALID_TOKEN);
+		}
+
+		return false;
 	}
 
 	private Jwt.Claims verifyAndGetClaims(String token) {

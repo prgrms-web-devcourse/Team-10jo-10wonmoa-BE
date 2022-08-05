1704674cd21719c3ef99a5ce5200e0bf44c77cb3
geuno <rlfrmsdh1@gmail.com>
2022-08-05 17:40:31 +0900

    feat: 검색 API 서비스 구현 & API 서비스에서 사용할 dto 생성

---
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/service/SearchAccountBookService.java
@@ -0,0 +1,71 @@
+package com.prgrms.tenwonmoa.domain.accountbook.service;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prgrms.tenwonmoa.domain.accountbook.Expenditure;
+import com.prgrms.tenwonmoa.domain.accountbook.Income;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.SearchAccountBookResponse;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.SearchAccountBookResponse.Result;
+import com.prgrms.tenwonmoa.domain.accountbook.repository.SearchAccountBookRepository;
+import com.prgrms.tenwonmoa.domain.category.CategoryType;
+import com.prgrms.tenwonmoa.domain.common.page.PageCustomRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SearchAccountBookService {
+
+	private final SearchAccountBookRepository repository;
+
+	public SearchAccountBookResponse searchAccountBooks(Long authenticatedId, SearchAccountBookCmd cmd,
+		PageCustomRequest pageRequest) {
+		List<Expenditure> expenditures = repository.searchExpenditures(cmd.getMinPrice(), cmd.getMaxPrice(),
+			cmd.getStart(), cmd.getEnd(), cmd.getContent(), cmd.getCategories(), authenticatedId, pageRequest);
+
+		List<Income> incomes = repository.searchIncomes(cmd.getMinPrice(), cmd.getMaxPrice(), cmd.getStart(),
+			cmd.getEnd(), cmd.getContent(), cmd.getCategories(), authenticatedId, pageRequest);
+
+		List<Result> results = combineAndSort(expenditures, incomes);
+
+		return sliceAndSum(results, pageRequest);
+	}
+
+	private List<Result> combineAndSort(List<Expenditure> expenditures, List<Income> incomes) {
+		List<Result> results = expenditures.stream()
+			.map(e -> new Result(e.getDate(), e.getAmount(), e.getContent(), e.getId(),
+				CategoryType.EXPENDITURE.name(), e.getCategoryName()))
+			.collect(Collectors.toList());
+
+		incomes.forEach(i ->
+			results.add(new Result(i.getDate(), i.getAmount(), i.getContent(),
+				i.getId(), CategoryType.INCOME.name(), i.getCategoryName())));
+
+		results.sort(Comparator.comparing(Result::getRegisterDate, LocalDate::compareTo));
+		return results;
+	}
+
+	private SearchAccountBookResponse sliceAndSum(List<Result> results, PageCustomRequest pageRequest) {
+		List<Result> slicedResult = results.subList(0, pageRequest.getSize());
+		Long incomeSum = 0L;
+		Long expenditureSum = 0L;
+
+		for (Result result : slicedResult) {
+			if (result.getType().equals(CategoryType.EXPENDITURE.name())) {
+				incomeSum += result.getAmount();
+			} else {
+				expenditureSum += result.getAmount();
+			}
+		}
+
+		return SearchAccountBookResponse.of(slicedResult, incomeSum, expenditureSum);
+	}
+
+}

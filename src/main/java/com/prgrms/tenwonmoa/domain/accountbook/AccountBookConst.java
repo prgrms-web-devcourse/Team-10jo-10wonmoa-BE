dd50d12c7cd1efe6ea83e3d610d1021a823a5fdb
geuno <rlfrmsdh1@gmail.com>
2022-08-05 13:02:14 +0900

    feat: 검색 API 위한 순수 Jpa 리포지토리 구현 & 테스트 작성

--- src/main/java/com/prgrms/tenwonmoa/domain/accountbook/AccountBookConst.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/AccountBookConst.java
@@ -1,5 +1,7 @@
 package com.prgrms.tenwonmoa.domain.accountbook;
 
+import java.time.LocalDateTime;
+
 public final class AccountBookConst {
 	private AccountBookConst() {
 	}
@@ -7,4 +9,8 @@ public final class AccountBookConst {
 	public static final Long AMOUNT_MAX = 1000000000000L;
 	public static final Long AMOUNT_MIN = 1L;
 	public static final int CONTENT_MAX = 50;
+
+	public static final LocalDateTime LEFT_MOST_REGISTER_DATE = LocalDateTime.of(2000, 1, 1, 0, 0);
+
+	public static final LocalDateTime RIGHT_MOST_REGISTER_DATE = LocalDateTime.of(3000, 12, 31, 23, 59);
 }

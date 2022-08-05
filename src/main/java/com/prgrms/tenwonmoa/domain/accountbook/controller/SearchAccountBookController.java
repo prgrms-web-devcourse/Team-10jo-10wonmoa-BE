e88a3778788a2a7eed0ee9c6a540f130dfa55b73
geuno <rlfrmsdh1@gmail.com>
2022-08-05 21:48:03 +0900

    feat: 컨트롤러 단위 테스트 작성

--- src/main/java/com/prgrms/tenwonmoa/domain/accountbook/controller/SearchAccountBookController.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/controller/SearchAccountBookController.java
@@ -1,10 +1,10 @@
 package com.prgrms.tenwonmoa.domain.accountbook.controller;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.prgrms.tenwonmoa.domain.accountbook.dto.SearchAccountBookResponse;
 import com.prgrms.tenwonmoa.domain.accountbook.service.SearchAccountBookCmd;
 import com.prgrms.tenwonmoa.domain.accountbook.service.SearchAccountBookService;
-import com.prgrms.tenwonmoa.domain.category.CategoryType;
-import com.prgrms.tenwonmoa.domain.category.dto.FindCategoryResponse;
 import com.prgrms.tenwonmoa.domain.category.service.FindUserCategoryService;
 import com.prgrms.tenwonmoa.domain.common.page.PageCustomRequest;
 
@@ -37,32 +35,27 @@ public class SearchAccountBookController {
 		@RequestParam(defaultValue = "") String categories,
 		@RequestParam(name = "minprice", required = false) Long minPrice,
 		@RequestParam(name = "maxprice", required = false) Long maxPrice,
-		@RequestParam(required = false) LocalDate start,
-		@RequestParam(required = false) LocalDate end,
+		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate start,
+		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end,
 		@RequestParam(defaultValue = "") String content,
 		@RequestParam(defaultValue = "10") int size,
 		@RequestParam(defaultValue = "1") int page) {
 
 		categories = categories.isEmpty() ? getAllCategories(userId) : categories;
 
+		PageCustomRequest pageRequest = new PageCustomRequest(page, size);
+
 		SearchAccountBookResponse response = accountBookService.searchAccountBooks(userId,
-			SearchAccountBookCmd.of(categories, minPrice, maxPrice, start, end, content),
-			new PageCustomRequest(page, size));
+			SearchAccountBookCmd.of(categories, minPrice, maxPrice, start, end, content), pageRequest);
+		response.setPageInfo(pageRequest);
 
 		return ResponseEntity.ok(response);
 	}
 
 	private String getAllCategories(Long userId) {
-		FindCategoryResponse expenditureCategories = userCategoryService.findUserCategories(userId,
-			CategoryType.EXPENDITURE.name());
-		FindCategoryResponse incomeCategories = userCategoryService.findUserCategories(userId,
-			CategoryType.INCOME.name());
+		List<Long> allUserCategories = userCategoryService.findAllUserCategoryIds(userId);
 
-		List<Long> categoryIds = new ArrayList<>();
-		expenditureCategories.getCategories().forEach(resp -> categoryIds.add(resp.getId()));
-		incomeCategories.getCategories().forEach(resp -> categoryIds.add(resp.getId()));
-
-		return String.join(",", categoryIds.stream()
+		return String.join(",", allUserCategories.stream()
 			.map(String::valueOf)
 			.collect(Collectors.toList())
 			.toArray(String[]::new));
---
+++ src/test/java/com/prgrms/tenwonmoa/domain/accountbook/controller/SearchAccountBookControllerTest.java
@@ -0,0 +1,110 @@
+package com.prgrms.tenwonmoa.domain.accountbook.controller;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.*;
+import static com.prgrms.tenwonmoa.domain.accountbook.dto.SearchAccountBookResponse.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.prgrms.tenwonmoa.common.annotation.WithMockCustomUser;
+import com.prgrms.tenwonmoa.config.JwtConfigure;
+import com.prgrms.tenwonmoa.config.WebSecurityConfig;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.SearchAccountBookResponse;
+import com.prgrms.tenwonmoa.domain.accountbook.service.SearchAccountBookCmd;
+import com.prgrms.tenwonmoa.domain.accountbook.service.SearchAccountBookService;
+import com.prgrms.tenwonmoa.domain.category.service.FindUserCategoryService;
+import com.prgrms.tenwonmoa.domain.common.page.PageCustomRequest;
+import com.prgrms.tenwonmoa.domain.user.security.jwt.filter.JwtAuthenticationFilter;
+
+@WebMvcTest(controllers = SearchAccountBookController.class,
+	excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfig.class),
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtConfigure.class),
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)
+	}
+)
+@MockBean(JpaMetamodelMappingContext.class)
+@DisplayName("가계부 검색 컨트롤러 테스트")
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+class SearchAccountBookControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private SearchAccountBookService accountBookService;
+
+	@MockBean
+	private FindUserCategoryService userCategoryService;
+
+	@Test
+	@WithMockCustomUser
+	void 지출_수입_검색() throws Exception {
+		SearchAccountBookResponse response = of(
+			List.of(
+				new Result(LocalDate.now(), 10000L, "점심", 1L, "EXPENDITURE", "식비"),
+				new Result(LocalDate.now(), 50000L, "용돈", 1L, "INCOME", "용돈")),
+			50000L, 10000L);
+
+		given(accountBookService.searchAccountBooks(anyLong(), any(SearchAccountBookCmd.class), any(
+			PageCustomRequest.class))).willReturn(response);
+
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/api/v1/account-book/search")
+				.param("categories", "1,2,3")
+				.param("minprice", "1000")
+				.param("maxprice", "50000")
+				.param("start", "2022-08-01")
+				.param("end", "2022-08-10")
+				.param("content", "점심")
+				.param("size", "1")
+				.param("page", "1"))
+			.andExpect(status().isOk())
+			.andDo(
+				document("search-account-book",
+					requestParameters(
+						parameterWithName("categories").description("유저카테고리 아이디"),
+						parameterWithName("minprice").description("최소 가격"),
+						parameterWithName("maxprice").description("최대 가격"),
+						parameterWithName("start").description("시작 등록일"),
+						parameterWithName("end").description("종료 등록일"),
+						parameterWithName("content").description("지출, 수입의 내용"),
+						parameterWithName("size").description("페이지의 사이즈"),
+						parameterWithName("page").description("페이지 번호")
+					),
+					responseFields(
+						fieldWithPath("incomeSum").type(JsonFieldType.NUMBER).description("수입의 총합"),
+						fieldWithPath("expenditureSum").type(JsonFieldType.NUMBER).description("지출의 총합"),
+						fieldWithPath("currentPage").type(JsonFieldType.NUMBER).description("현재 페이지"),
+						fieldWithPath("nextPage").type(JsonFieldType.NUMBER).description("다음 페이지"),
+						fieldWithPath("results[]").type(JsonFieldType.ARRAY).description("검색된 지출, 수입 데이터"),
+						fieldWithPath("results[].registerDate").type(JsonFieldType.STRING).description("등록일"),
+						fieldWithPath("results[].amount").type(JsonFieldType.NUMBER).description("금액"),
+						fieldWithPath("results[].content").type(JsonFieldType.STRING).description("내용"),
+						fieldWithPath("results[].id").type(JsonFieldType.NUMBER).description("지출 or 수입의 아이디"),
+						fieldWithPath("results[].type").type(JsonFieldType.STRING).description("지출 or 수입 종류"),
+						fieldWithPath("results[].categoryName").type(JsonFieldType.STRING).description("카테고리 이름")
+					)
+				)
+			);
+	}
+}

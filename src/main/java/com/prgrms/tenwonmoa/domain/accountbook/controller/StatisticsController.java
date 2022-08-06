5e7dab8b95971ae97b8cf1ad44c931e2d74b713c
taesan <pawer02023@gmail.com>
2022-08-07 01:26:13 +0900

    feat: 통계 조회 Controller 구현
    - 통계 조회 Controller 구현

---
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/controller/StatisticsController.java
@@ -0,0 +1,35 @@
+package com.prgrms.tenwonmoa.domain.accountbook.controller;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prgrms.tenwonmoa.domain.accountbook.dto.statistics.FindStatisticsResponse;
+import com.prgrms.tenwonmoa.domain.accountbook.service.StatisticsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/statistics")
+public class StatisticsController {
+	private final StatisticsService statisticsService;
+
+	@GetMapping
+	public ResponseEntity<FindStatisticsResponse> findStatistics(
+		@AuthenticationPrincipal Long userId,
+		@RequestParam @NotBlank @Min(1000) Integer year,
+		@RequestParam(required = false) @Min(1) @Max(31) Integer month
+	) {
+		return ResponseEntity.ok(statisticsService.searchStatistics(userId,
+			year,
+			month));
+	}
+}

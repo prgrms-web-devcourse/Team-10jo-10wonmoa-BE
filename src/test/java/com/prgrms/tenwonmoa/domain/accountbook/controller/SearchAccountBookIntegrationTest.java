8c6ee6f2ab28480c33dfba25fa929c6da22276c4
geuno <rlfrmsdh1@gmail.com>
2022-08-06 23:51:12 +0900

    feat: 테스트 케이스 추가

--- src/test/java/com/prgrms/tenwonmoa/domain/accountbook/controller/SearchAccountBookIntegrationTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/accountbook/controller/SearchAccountBookIntegrationTest.java
@@ -12,13 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.prgrms.tenwonmoa.common.BaseControllerIntegrationTest;
-import com.prgrms.tenwonmoa.domain.accountbook.dto.income.CreateIncomeRequest;
-import com.prgrms.tenwonmoa.domain.category.dto.CreateCategoryRequest;
 
 @DisplayName("검색 컨트롤러 통합 테스트")
 @Transactional
@@ -80,51 +76,19 @@ class SearchAccountBookIntegrationTest extends BaseControllerIntegrationTest {
 			.andExpect(jsonPath("$.results", hasSize(2)));
 	}
 
-	private void 지출_등록(Long userCategoryId, Long amount,
-		String content, LocalDateTime registerDate) throws Exception {
+	@Test
+	void 검색_Api_디폴트_조회() throws Exception {
+		Long incomeCategoryId = 카테고리_등록("INCOME", "월급");
+		Long expenditureCategoryId = 카테고리_등록("EXPENDITURE", "식비");
+		수입_등록(incomeCategoryId, 10000L, "용돈", LocalDateTime.now());
+		지출_등록(expenditureCategoryId, 5000L, "점심", LocalDateTime.now());
 
-		ObjectNode objectNode = objectMapper.createObjectNode();
-		objectNode.put("registerDate", registerDate.toString());
-		objectNode.put("amount", amount)
-			.put("content", content)
-			.put("userCategoryId", userCategoryId);
-
-		mvc.perform(post("/api/v1/expenditures")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectNode.toString())
+		mvc.perform(get("/api/v1/account-book/search")
 				.header(HttpHeaders.AUTHORIZATION, accessToken))
-			.andExpect(status().isCreated())
-			.andExpect(jsonPath("id").exists());
-	}
-
-	private void 수입_등록(Long userCategoryId, Long amount,
-		String content, LocalDateTime registerDate) throws Exception {
-		CreateIncomeRequest request = new CreateIncomeRequest(registerDate, amount, content,
-			userCategoryId);
-
-		mvc.perform(post("/api/v1/incomes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(request))
-				.header(HttpHeaders.AUTHORIZATION, accessToken))
-			.andExpect(status().isCreated())
-			.andExpect(jsonPath("id").exists());
-	}
-
-	private Long 카테고리_등록(String categoryType, String categoryName) throws Exception {
-		//given
-		CreateCategoryRequest createCategoryRequest
-			= new CreateCategoryRequest(categoryType, categoryName);
-
-		//when
-		//then
-		String response = mvc.perform(post("/api/v1/categories")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(createCategoryRequest))
-				.header(HttpHeaders.AUTHORIZATION, accessToken))
-			.andExpectAll(
-				status().isCreated(),
-				jsonPath("$.id").exists()
-			)
-			.andReturn().getResponse().getContentAsString();
-		return objectMapper.readTree(response).get("id").asLong();
+			.andExpect(jsonPath("$.incomeSum").value(10000L))
+			.andExpect(jsonPath("$.expenditureSum").value(5000L))
+			.andExpect(jsonPath("$.currentPage").value(1))
+			.andExpect(jsonPath("$.nextPage").isEmpty())
+			.andExpect(jsonPath("$.results", hasSize(2)));
 	}
 }

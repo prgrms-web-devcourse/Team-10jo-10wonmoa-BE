8582d9bb64a753c99c2d529efd3a5677fb50029d
jki503 <jki503@naver.com>
2022-08-07 19:56:48 +0900

    test: 월별 상세내역 조회 테스트 추가, 미래, 과거, 현재(미래월까지 작성), 현재(현재 월까지 작성)

--- src/test/java/com/prgrms/tenwonmoa/domain/accountbook/repository/AccountBookQueryRepositoryTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/accountbook/repository/AccountBookQueryRepositoryTest.java
@@ -18,7 +18,10 @@ import com.prgrms.tenwonmoa.common.RepositoryTest;
 import com.prgrms.tenwonmoa.domain.accountbook.Expenditure;
 import com.prgrms.tenwonmoa.domain.accountbook.Income;
 import com.prgrms.tenwonmoa.domain.accountbook.dto.FindDayAccountResponse;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.FindMonthAccountResponse;
 import com.prgrms.tenwonmoa.domain.accountbook.dto.FindSumResponse;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.MonthCondition;
+import com.prgrms.tenwonmoa.domain.accountbook.dto.MonthDetail;
 import com.prgrms.tenwonmoa.domain.category.Category;
 import com.prgrms.tenwonmoa.domain.category.UserCategory;
 import com.prgrms.tenwonmoa.domain.common.page.PageCustomImpl;
@@ -62,8 +65,8 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 
 		@Test
 		public void 수입과_지출_모두_존재할경우() {
-			createExpenditures(10);
-			createIncomes(10);
+			createExpenditures(10, 2022, 8);
+			createIncomes(10, 2022, 8);
 
 			FindSumResponse monthSum = accountBookQueryRepository.findMonthSum(user.getId(),
 				LocalDate.parse("2022-08-01", DateTimeFormatter.ofPattern("yyyy-MM-dd")));
@@ -75,7 +78,7 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 
 		@Test
 		public void 수입만_존재할경우() {
-			createIncomes(7);
+			createIncomes(7, 2022, 8);
 
 			FindSumResponse monthSum = accountBookQueryRepository.findMonthSum(user.getId(),
 				LocalDate.parse("2022-08-01", DateTimeFormatter.ofPattern("yyyy-MM-dd")));
@@ -87,7 +90,7 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 
 		@Test
 		public void 지출만_존재할경우() {
-			createExpenditures(8);
+			createExpenditures(8, 2022, 8);
 
 			FindSumResponse monthSum = accountBookQueryRepository.findMonthSum(user.getId(),
 				LocalDate.parse("2022-08-01", DateTimeFormatter.ofPattern("yyyy-MM-dd")));
@@ -114,8 +117,8 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 	class FindYearSumQuery {
 		@Test
 		public void 수입과_지출_모두_존재할경우() {
-			createExpenditures(10);
-			createIncomes(10);
+			createExpenditures(10, 2022, 8);
+			createIncomes(10, 2022, 8);
 
 			FindSumResponse monthSum = accountBookQueryRepository.findYearSum(user.getId(), 2022);
 
@@ -126,7 +129,7 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 
 		@Test
 		public void 수입만_존재할경우() {
-			createIncomes(7);
+			createIncomes(7, 2022, 8);
 
 			FindSumResponse yearSum = accountBookQueryRepository.findYearSum(user.getId(), 2022);
 
@@ -137,7 +140,7 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 
 		@Test
 		public void 지출만_존재할경우() {
-			createExpenditures(8);
+			createExpenditures(8, 2022, 8);
 
 			FindSumResponse yearSum = accountBookQueryRepository.findYearSum(user.getId(), 2022);
 
@@ -178,8 +181,8 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 
 		@Test
 		public void 날짜를_수입과_집합의_날짜_합집합을_10개_반환하여_페이징처리한다() {
-			createExpenditures(10);
-			createIncomes(10);
+			createExpenditures(10, 2022, 8);
+			createIncomes(10, 2022, 8);
 
 			PageCustomImpl<FindDayAccountResponse> dailyAccount = accountBookQueryRepository.findDailyAccount(
 				user.getId(),
@@ -204,8 +207,8 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 
 		@Test
 		public void 마지막_페이지일_경우_응답에서_nextPage를_null로_응답한다() {
-			createExpenditures(10);
-			createIncomes(10);
+			createExpenditures(10, 2022, 8);
+			createIncomes(10, 2022, 8);
 
 			PageCustomImpl<FindDayAccountResponse> dailyAccount = accountBookQueryRepository.findDailyAccount(
 				user.getId(),
@@ -222,10 +225,198 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 		}
 	}
 
-	private void createExpenditures(int count) {
+	@Nested
+	@DisplayName("월별 상세내역 조회")
+	class FindMonthAccount {
+
+		LocalDateTime now = LocalDateTime.of(2022, 8, 1, 0, 0);
+		int currentYear = 2022;
+		int pastYear = 2021;
+		int futureYear = 2023;
+
+		@Test
+		public void 현재_년도2022에서_현재월8월보다_작성한내역이_과거일때는_8월부터_1월까지_내역을_보여준다() {
+
+			/**
+			 * given
+			 * 8월 지출: 0, 수입: 0
+			 * 7월 지출: 2000, 수입: 0
+			 * 6월 지출: 2000, 수입: 2000
+			 * 5월 지출: 2000, 수입: 2000
+			 * 4월 지출: 0, 수입: 2000
+			 * 3월 지출: 0, 수입: 2000
+			 * 2월 지출: 0, 수입: 0
+			 * 1월 지출: 0, 수입: 0
+			 */
+			createExpenditures(2, currentYear, 7);
+			createExpenditures(2, currentYear, 6);
+			createExpenditures(2, currentYear, 5);
+			createIncomes(2, currentYear, 6);
+			createIncomes(2, currentYear, 5);
+			createIncomes(2, currentYear, 4);
+			createIncomes(2, currentYear, 3);
+
+			//when
+			FindMonthAccountResponse monthAccount = accountBookQueryRepository.findMonthAccount(user.getId(),
+				new MonthCondition(now, currentYear));
+
+			List<MonthDetail> results = monthAccount.getResults();
+
+			for (int i = 8; i >= 1; i--) {
+				MonthDetail monthDetail = results.get(8 - i);
+
+				if (i == 7) {
+					checkMonthDetail(monthDetail, i, 2000L, 0L, -2000L);
+				}
+
+				if (i == 6 || i == 5) {
+					checkMonthDetail(monthDetail, i, 2000L, 2000L, 0L);
+				}
+
+				if (i == 4 && i <= 3) {
+					checkMonthDetail(monthDetail, i, 0L, 2000L, 2000L);
+				}
+
+				if (i <= 2 || i == 8) {
+					checkMonthDetail(monthDetail, i, 0L, 0L, 0L);
+				}
+			}
+		}
+
+		@Test
+		public void 현재_년도2022에서_현재월8월보다_작성한내역_미래11월이_있을때는_11월부터_1월까지_내역을_보여준다() {
+			/**
+			 * given
+			 * 11월 지출: 4000, 수입: 3000
+			 * 10월 지출: 0, 수입: 0
+			 * 9월 지출: 0, 수입: 0
+			 * 08월 지출: 2000, 수입: 0
+			 * 07월 지출: 2000, 수입: 0
+			 * 06월 지출: 2000, 수입: 2000
+			 * 05월 지출: 2000, 수입: 2000
+			 * 04월 지출: 0, 수입: 2000
+			 * 03월 지출: 0, 수입: 2000
+			 * 02월 지출: 0, 수입: 0
+			 * 01월 지출: 0, 수입: 0
+			 */
+			createExpenditures(4, currentYear, 11);
+			createIncomes(3, currentYear, 11);
+			createExpenditures(2, currentYear, 8);
+			createExpenditures(2, currentYear, 7);
+			createExpenditures(2, currentYear, 6);
+			createExpenditures(2, currentYear, 5);
+			createIncomes(2, currentYear, 6);
+			createIncomes(2, currentYear, 5);
+			createIncomes(2, currentYear, 4);
+			createIncomes(2, currentYear, 3);
+
+			//when
+			FindMonthAccountResponse monthAccount = accountBookQueryRepository.findMonthAccount(user.getId(),
+				new MonthCondition(now, currentYear));
+
+			List<MonthDetail> results = monthAccount.getResults();
+
+			for (int i = 11; i >= 1; i--) {
+				MonthDetail monthDetail = results.get(11 - i);
+
+				if (i == 11) {
+					checkMonthDetail(monthDetail, i, 4000L, 3000L, -1000L);
+				}
+
+				if (i == 8 || i == 7) {
+					checkMonthDetail(monthDetail, i, 2000L, 0L, -2000L);
+				}
+
+				if (i == 6 || i == 5) {
+					checkMonthDetail(monthDetail, i, 2000L, 2000L, 0L);
+				}
+
+				if (i == 4 && i <= 3) {
+					checkMonthDetail(monthDetail, i, 0L, 2000L, 2000L);
+				}
+
+				if (i <= 2 || i == 10 || i == 9) {
+					checkMonthDetail(monthDetail, i, 0L, 0L, 0L);
+				}
+			}
+		}
+
+		@Test
+		public void 현재_년도보다_과거년도를_조회할때는_12월에서_1월까지_모두보여준다() {
+			/**
+			 *	given
+			 * 12~8월, 4~1월은 지출과 수입 모두 0원
+			 * 7월 지출: 2000, 수입: 0
+			 * 6월 지출: 2000, 수입: 2000
+			 * 5월 지출: 2000, 수입: 2000
+			 */
+			createExpenditures(2, pastYear, 7);
+			createExpenditures(2, pastYear, 6);
+			createExpenditures(2, pastYear, 5);
+			createIncomes(2, pastYear, 6);
+			createIncomes(2, pastYear, 5);
+
+			//when
+			FindMonthAccountResponse monthAccount = accountBookQueryRepository.findMonthAccount(user.getId(),
+				new MonthCondition(now, pastYear));
+
+			List<MonthDetail> results = monthAccount.getResults();
+
+			for (int i = 12; i >= 1; i--) {
+				MonthDetail monthDetail = results.get(12 - i);
+
+				if (i == 7) {
+					checkMonthDetail(monthDetail, i, 2000L, 0L, -2000L);
+					continue;
+				}
+
+				if (i == 6 || i == 5) {
+					checkMonthDetail(monthDetail, i, 2000L, 2000L, 0L);
+					continue;
+				}
+
+				checkMonthDetail(monthDetail, i, 0L, 0L, 0L);
+			}
+
+		}
+
+		@Test
+		public void 현재_년도보다_미래년도를_조회할때는_작성한_월만_보여준다() {
+			/**
+			 *	given
+			 * 7월 지출: 2000, 수입: 0
+			 * 6월 지출: 2000, 수입: 2000
+			 * 5월 지출: 2000, 수입: 2000
+			 */
+			createExpenditures(2, futureYear, 7);
+			createExpenditures(2, futureYear, 6);
+			createExpenditures(2, futureYear, 5);
+			createIncomes(2, futureYear, 6);
+			createIncomes(2, futureYear, 5);
+
+			//when
+			FindMonthAccountResponse monthAccount = accountBookQueryRepository.findMonthAccount(user.getId(),
+				new MonthCondition(now, futureYear));
+
+			List<MonthDetail> results = monthAccount.getResults();
+
+			for (int i = 7; i >= 5; i--) {
+				MonthDetail monthDetail = results.get(7 - i);
+
+				if (i == 7) {
+					checkMonthDetail(monthDetail, i, 2000L, 0L, -2000L);
+					continue;
+				}
+
+				checkMonthDetail(monthDetail, i, 2000L, 2000L, 0L);
+			}
+		}
+	}
+
+	private void createExpenditures(int count, int year, int month) {
 		for (int i = 0; i < count; i++) {
 			expenditureRepository.save(new Expenditure(
-				LocalDateTime.of(2022, 8, 1 + i * 2, 0, 0),
+				LocalDateTime.of(year, month, 1 + i * 2, 0, 0),
 				1000L,
 				"지출" + i,
 				expenditureCategory.getName(),
@@ -235,10 +426,10 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 		}
 	}
 
-	private void createIncomes(int count) {
+	private void createIncomes(int count, int year, int month) {
 		for (int i = 0; i < count; i++) {
 			incomeRepository.save(new Income(
-				LocalDateTime.of(2022, 8, 11 + i * 2, 0, 0),
+				LocalDateTime.of(year, month, 11 + i * 2, 0, 0),
 				1000L,
 				"수입" + i,
 				incomeCategory.getName(),
@@ -247,5 +438,14 @@ class AccountBookQueryRepositoryTest extends RepositoryTest {
 			));
 		}
 	}
+
+	private void checkMonthDetail(MonthDetail monthDetail, int targetMonth, Long targetExpenditure, Long targetIncome,
+		Long targetTotal) {
+		assertThat(monthDetail.getMonth()).isEqualTo(targetMonth);
+		assertThat(monthDetail.getIncomeSum()).isEqualTo(targetIncome);
+		assertThat(monthDetail.getExpenditureSum()).isEqualTo(targetExpenditure);
+		assertThat(monthDetail.getTotalSum()).isEqualTo(targetTotal);
+	}
+
 }
 

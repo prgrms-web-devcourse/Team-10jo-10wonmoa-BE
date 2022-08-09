edbdbff2c22a4cf92a8319ecffbe5f1483c3452b
geuno <rlfrmsdh1@gmail.com>
2022-08-09 21:24:04 +0900

    feat: service에 validation 로직 추가

--- src/test/java/com/prgrms/tenwonmoa/domain/accountbook/service/SearchAccountBookServiceTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/accountbook/service/SearchAccountBookServiceTest.java
@@ -5,6 +5,7 @@ import static com.prgrms.tenwonmoa.domain.accountbook.AccountBookConst.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -60,7 +61,7 @@ class SearchAccountBookServiceTest {
 		AccountBookItem thirdExpenditure = new AccountBookItem(2L, CategoryType.EXPENDITURE.name(), 20000L,
 			"영화", cultureCategory.getCategoryName(), LocalDateTime.now().minusDays(1L));
 
-		SearchAccountBookCmd cmd = SearchAccountBookCmd.of("1,2,3", AMOUNT_MAX, AMOUNT_MIN,
+		SearchAccountBookCmd cmd = SearchAccountBookCmd.of("1,2,3", AMOUNT_MIN, AMOUNT_MAX,
 			LEFT_MOST_REGISTER_DATE, RIGHT_MOST_REGISTER_DATE, "");
 		PageCustomRequest pageRequest = new PageCustomRequest(1, 3);
 
@@ -80,4 +81,31 @@ class SearchAccountBookServiceTest {
 			.isEqualTo(latestIncome.getAmount() - secondExpenditure.getAmount() - thirdExpenditure.getAmount());
 	}
 
+	@Test
+	void 금액_최소값이_최대값보다_크면_검색_실패() {
+		Long minPrice = AMOUNT_MAX;
+		Long maxPrice = AMOUNT_MIN;
+
+		SearchAccountBookCmd cmd = SearchAccountBookCmd.of("1,2,3", minPrice, maxPrice,
+			LEFT_MOST_REGISTER_DATE, RIGHT_MOST_REGISTER_DATE, "");
+		PageCustomRequest pageRequest = new PageCustomRequest(1, 3);
+
+		assertThatIllegalArgumentException().isThrownBy(
+			() -> accountBookService.searchAccountBooks(userId, cmd, pageRequest)
+		);
+	}
+
+	@Test
+	void 시작일이_종료일보다_뒤이면_검색_실패() {
+		LocalDate start = RIGHT_MOST_REGISTER_DATE;
+		LocalDate end = LEFT_MOST_REGISTER_DATE;
+
+		SearchAccountBookCmd cmd = SearchAccountBookCmd.of("1,2,3", AMOUNT_MIN, AMOUNT_MAX,
+			start, end, "");
+		PageCustomRequest pageRequest = new PageCustomRequest(1, 3);
+
+		assertThatIllegalArgumentException().isThrownBy(
+			() -> accountBookService.searchAccountBooks(userId, cmd, pageRequest)
+		);
+	}
 }

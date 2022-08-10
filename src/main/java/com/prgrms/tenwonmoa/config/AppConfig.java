d97c48e20b70e29d11c14ed921166b4360c70955
Yanjoo <hanjub3@gmail.com>
2022-08-11 05:22:56 +0900

    feat: 승인된 redirect-url 속성 추가

---
+++ src/main/java/com/prgrms/tenwonmoa/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.prgrms.tenwonmoa.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
+public class AppConfig {
+	private List<String> authorizedRedirectUris = new ArrayList<>();
+}

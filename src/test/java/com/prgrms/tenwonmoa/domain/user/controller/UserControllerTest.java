456ac3e024f46e1ba06e4a4fb735dfac2e2c7f4d
Yanjoo <hanjub3@gmail.com>
2022-08-10 17:30:19 +0900

    refactor: 테스트 코드에 excludeFilter 추가

--- src/test/java/com/prgrms/tenwonmoa/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/user/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -11,29 +12,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prgrms.tenwonmoa.common.annotation.WithMockCustomUser;
+import com.prgrms.tenwonmoa.config.JwtConfigure;
+import com.prgrms.tenwonmoa.config.WebSecurityConfig;
 import com.prgrms.tenwonmoa.domain.user.dto.CreateUserRequest;
 import com.prgrms.tenwonmoa.domain.user.security.jwt.filter.JwtAuthenticationFilter;
 import com.prgrms.tenwonmoa.domain.user.service.UserService;
 
-@WebMvcTest(UserController.class)
-@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(controllers = UserController.class,
+	excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfig.class),
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtConfigure.class),
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)
+	}
+)
 @AutoConfigureRestDocs
 @MockBean(JpaMetamodelMappingContext.class)
 @DisplayName("유저 컨트롤러 테스트")
 class UserControllerTest {
-
-	@MockBean
-	private JwtAuthenticationFilter jwtAuthenticationFilter;    // 테스트 실행을 위해 필요
-
 	@MockBean
 	private UserService userService;
 
@@ -44,6 +50,7 @@ class UserControllerTest {
 	private ObjectMapper objectMapper;
 
 	@Test
+	@WithMockCustomUser
 	void 회원가입_성공() throws Exception {
 		CreateUserRequest createUserRequest =
 			new CreateUserRequest("test@test.com", "lee", "12345678");
@@ -52,6 +59,7 @@ class UserControllerTest {
 		given(userService.createUser(createUserRequest)).willReturn(userId);
 
 		mockMvc.perform(post("/api/v1/users")
+				.with(csrf())
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(createUserRequest)))

26e04fab48dd8a3c9b36e204e1611496167f8a9e
jki503 <jki503@naver.com>
2022-08-11 16:01:53 +0900

    fix: @validated 삭제 어차피 calendarCondition에서 검증

--- src/main/java/com/prgrms/tenwonmoa/domain/accountbook/controller/AccountBookQueryController.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/accountbook/controller/AccountBookQueryController.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -80,8 +79,7 @@ public class AccountBookQueryController {
 	@GetMapping("/calendar")
 	public ResponseEntity<FindCalendarResponse> findCalendarAccount(
 		@AuthenticationPrincipal Long userId,
-		@RequestParam(value = "year")
-		@Validated int year,
+		@RequestParam(value = "year") int year,
 		@RequestParam("month") int month
 	) {
 		CalendarCondition condition = new CalendarCondition(year, month);

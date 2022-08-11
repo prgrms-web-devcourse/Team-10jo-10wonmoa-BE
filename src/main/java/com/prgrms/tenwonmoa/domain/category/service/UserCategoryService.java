21b1c48191d951d0264c90df02ddc414b5a59b49
geuno <rlfrmsdh1@gmail.com>
2022-08-11 13:00:52 +0900

    feat: 유저카테고리 테이블 필드 변경 sql 추가 & validation 로직 위치 변경

--- src/main/java/com/prgrms/tenwonmoa/domain/category/service/UserCategoryService.java
+++ src/main/java/com/prgrms/tenwonmoa/domain/category/service/UserCategoryService.java
@@ -67,11 +67,12 @@ public class UserCategoryService {
 		Category category = userCategory.getCategory();
 		userCategory.updateCategoryAsNull();
 
-		makeCategoryNameUpToDate(userCategoryId, category.getName());
-
 		checkNotNull(category, String.format(
 			"삭제된 카테고리는 다시 삭제할 수 없습니다, 유저 아이디 : %d, 유저 카테고리 아이디 : %d",
 			authenticatedUser.getId(), userCategoryId));
+
+		makeCategoryNameUpToDate(userCategoryId, category.getName());
+
 		categoryService.deleteCategory(category.getId());
 	}
 

2f71e7aa7d4b381c9a81ed7f4b232c7f3b0241fa
taesan <pawer02023@gmail.com>
2022-08-12 23:39:23 +0900

    feat: 예산 통계조회 컨트롤러 테스트 작성
    - 예산 통계조회 컨트롤러 테스트 작성

--- src/test/java/com/prgrms/tenwonmoa/domain/budget/controller/docs/BudgetControllerTest.java
+++ src/test/java/com/prgrms/tenwonmoa/domain/budget/controller/docs/BudgetControllerTest.java
@@ -1,15 +1,18 @@
 package com.prgrms.tenwonmoa.domain.budget.controller.docs;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.*;
+import static com.prgrms.tenwonmoa.common.documentdto.FindBudgetWithExpenditureResponseDoc.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.YearMonth;
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,12 +29,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.tenwonmoa.common.annotation.WithMockCustomUser;
 import com.prgrms.tenwonmoa.common.documentdto.CreateOrUpdateBudgetRequestDoc;
+import com.prgrms.tenwonmoa.common.documentdto.FindBudgetByRegisterDateDoc;
 import com.prgrms.tenwonmoa.common.documentdto.FindBudgetDataDoc;
 import com.prgrms.tenwonmoa.config.JwtConfigure;
 import com.prgrms.tenwonmoa.config.WebSecurityConfig;
 import com.prgrms.tenwonmoa.domain.budget.controller.BudgetController;
 import com.prgrms.tenwonmoa.domain.budget.dto.CreateOrUpdateBudgetRequest;
+import com.prgrms.tenwonmoa.domain.budget.dto.FindBudgetByRegisterDate;
 import com.prgrms.tenwonmoa.domain.budget.dto.FindBudgetData;
+import com.prgrms.tenwonmoa.domain.budget.dto.FindBudgetWithExpenditureResponse;
 import com.prgrms.tenwonmoa.domain.budget.service.BudgetTotalService;
 import com.prgrms.tenwonmoa.domain.user.security.jwt.filter.JwtAuthenticationFilter;
 
@@ -57,11 +63,16 @@ class BudgetControllerTest {
 
 	private CreateOrUpdateBudgetRequest createOrUpdateBudgetRequest = new CreateOrUpdateBudgetRequest(
 		1000L, YearMonth.now(), 1L);
-
 	private List<FindBudgetData> findBudgets = List.of(
 		new FindBudgetData(1L, "교통/차량", 1000L),
 		new FindBudgetData(2L, "문화생활", 2000L),
 		new FindBudgetData(3L, "마트/편의점", 3000L)
+	);
+
+	private List<FindBudgetByRegisterDate> budgets = List.of(
+		new FindBudgetByRegisterDate(1L, "교통/차량", 100L),
+		new FindBudgetByRegisterDate(2L, "문화생활", 200L),
+		new FindBudgetByRegisterDate(3L, "마트/편의점", 300L)
 	);
 
 	@Test
@@ -94,4 +105,77 @@ class BudgetControllerTest {
 				responseFields().andWithPrefix("budgets[].", FindBudgetDataDoc.fieldDescriptors())
 			));
 	}
+
+	@Test
+	@WithMockCustomUser
+	void 월별_예산_통계조회_성공() throws Exception {
+		given(budgetTotalService.searchBudgetWithExpenditure(any(), any(), any()))
+			.willReturn(getFindBudgetWithExpenditureResponse(2022, 07));
+		mockMvc.perform(get(LOCATION_PREFIX + "/statistics")
+				.param("year", "2022")
+				.param("month", "7")
+			)
+			.andDo(print())
+			.andDo(document("budget_statistics-month",
+				responseFields(fieldDescriptors())
+					.andWithPrefix("budgets[].", FindBudgetByRegisterDateDoc.fieldDescriptors())
+			))
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithMockCustomUser
+	void 연별_예산_통계조회_성공() throws Exception {
+		given(budgetTotalService.searchBudgetWithExpenditure(any(), any(), any()))
+			.willReturn(getFindBudgetWithExpenditureResponse(2022, 07));
+		mockMvc.perform(get(LOCATION_PREFIX + "/statistics")
+				.param("year", "2022")
+			)
+			.andDo(print())
+			.andDo(document("budget_statistics-year",
+				responseFields(fieldDescriptors())
+					.andWithPrefix("budgets[].", FindBudgetByRegisterDateDoc.fieldDescriptors())
+			))
+			.andExpect(status().isOk());
+	}
+
+	private FindBudgetWithExpenditureResponse getFindBudgetWithExpenditureResponse(int year, int month) {
+		Long expenditure = 0L;
+		long amountSum = 0L;
+		long expenditureSum = 0L;
+
+		for (FindBudgetByRegisterDate budget : budgets) {
+			expenditure += 10L;
+			amountSum += budget.getAmount();
+			expenditureSum += expenditure;
+			budget.setExpenditure(expenditure);
+			budget.setPercent(calcPercent(budget.getAmount(), expenditure));
+		}
+		return new FindBudgetWithExpenditureResponse(
+			makeRegisterDate(year, month),
+			amountSum,
+			expenditureSum,
+			calcPercent(amountSum, expenditureSum),
+			budgets
+		);
+	}
+
+	private String makeRegisterDate(Integer year, Integer month) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(year);
+		if (Objects.nonNull(month)) {
+			sb.append("-").append(month);
+		}
+		return sb.toString();
+	}
+
+	private Long calcPercent(Long amount, Long expenditure) {
+		Long percent = 0L;
+		if (expenditure > 0) {
+			double doubleData = ((double)expenditure / amount) * 100;
+			percent = (long)Math.floor(doubleData);
+		}
+		return percent;
+	}
+
 }

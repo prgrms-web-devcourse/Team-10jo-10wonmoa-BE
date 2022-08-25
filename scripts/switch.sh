75181abbc7e6af02b0666a3fb72c7754af770ff7
mountain <pawer02023@gmail.com>
2022-08-26 02:02:05 +0900

    fix: 포트전환시 localhost ip로 변경
    포트전환시 localhost ip로 변경

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,7 +9,7 @@ function switch_proxy() {
 
   echo "> 전환할 Port: $IDLE_PORT"
   echo "> Port 전환"
-  echo "set \$service_url http://localhost:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  echo "set \$service_url http://3.39.184.232:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
   echo "> 엔진엑스 Reload"
   sudo service nginx reload

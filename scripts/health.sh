c7e0011ed97eda043ff516f03b8846f7f8129ca2
mountain <pawer02023@gmail.com>
2022-08-25 23:52:03 +0900

    chore: space 제거
    chore: space 제거

--- scripts/health.sh
+++ scripts/health.sh
@@ -17,7 +17,7 @@ do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo $RESPONSE} | grep 'dev' | wc -l)
 
-  if [ ${UP_COUNT} - ge 1 ]
+  if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 (dev 문자열이 있는지 검증)
     echo "> Health check 성공"
     switch_proxy
@@ -27,7 +27,7 @@ do
     echo "> Health check: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} - eq 10 ]
+  if [ ${RETRY_COUNT} -eq 10 ]
   then
     echo "> Health check 실패."
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."

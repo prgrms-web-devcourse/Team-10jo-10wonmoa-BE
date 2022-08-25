2f16e2344e0028cfb463b81c236520d92ba9d5c7
mountain <pawer02023@gmail.com>
2022-08-26 01:42:07 +0900

    fix: pid 확인 후 종료 프로세스 제거
    pid 확인 후 종료 프로세스 제거

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,18 +11,6 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션 없음."
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
-
 echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
